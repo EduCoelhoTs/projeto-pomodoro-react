f8900b46bccbf1fd6023d1c59f90ea9bd643fc5d
Eduardo Silva <edusilvatele@gmail.com>
2023-08-23 11:46:03 -0300

    Configurando rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      {/* adicionando global style na aplicação */}
-      <GlobalStyle />
+      {/* É necessário adicionar o BrowserRouter para que funcione o roteamento no navegador */}
+      <BrowserRouter>
+        <Router />
+        {/* adicionando global style na aplicação */}
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }

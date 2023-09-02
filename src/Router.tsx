c7cf542abbad0adc4d17f97d05455a045bb29bad
Eduardo Silva <edusilvatele@gmail.com>
2023-09-02 08:34:38 -0300

    Router e layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/home'
 import { History } from './pages/History'
+import { Home } from './pages/Home'
+import { DefaultLayout } from './layouts/DefaultLayouts'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        {/* O react vai renderizar dentro do Default layout, todos os componentes declarados dentro dele */}
+        {/* Sao somadas as rotas */}
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }

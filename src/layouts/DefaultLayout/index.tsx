ad356da78b640ac403ff2e662575e0979bfc2641
Eduardo Silva <edusilvatele@gmail.com>
2023-09-02 09:21:41 -0300

    componente header e routerlink

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { LayoutContainer } from './styles'
+import { Header } from '../../components/header'
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
       {/* outlet vai renderizar o componente filho. */}
-    </div>
+    </LayoutContainer>
   )
 }
